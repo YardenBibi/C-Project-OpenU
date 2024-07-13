de3a68a67f13df10f8e131fc96cc2495a3f298a2
YardenBibi <BBYARDEN@GMAIL.COM>
2024-07-13 16:32:17 +0300

    Create assembler.c

---
+++ SourceFiles/assembler.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (int argc, cher *argv[]){
+  int i; 
+  for (i=1; i<argc;i++){
+    printf("handle as files (split the macros)");
+    printf("countinue if fails");
+    printf("Perform first+second pass");
+  
+  }
