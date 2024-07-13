cfca78d8e165c90bb235fdfed61d334e6de094b4
YardenBibi <BBYARDEN@GMAIL.COM>
2024-07-13 17:24:37 +0300

    Update assembler.c

--- SourceFiles/assembler.c
+++ SourceFiles/assembler.c
@@ -5,8 +5,8 @@
 int main (int argc, cher *argv[]){
   int i; 
   for (i=1; i<argc;i++){
-    printf("handle as files (split the macros)");
-    printf("countinue if fails");
+    printf("handle .as files (split the macros)");
+    printf("continue if fails");
     printf("Perform first+second pass");
   
   }
